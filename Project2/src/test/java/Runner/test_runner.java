3582af0a7ab05d891645801d617e7ea589489f25
029940744 <029940744@nkogileg.in.ibm.com>
2021-04-20 09:27:54 +0530

    FirstProject

--- Project2/src/test/java/Runner/test_runner.java
+++ Project2/src/test/java/Runner/test_runner.java
@@ -6,8 +6,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/FirstProject/FirstProject.feature"},glue = {"stepsdefinition"}
-//plugin = {"html:testoutput/kogile.html","junit:testoutput/kogile.xml","json:testoutput/kogile.json"}
+@CucumberOptions(features = {"src/test/resources/FirstProject/FirstProject.feature"},glue = {"stepsdefinition"},
+plugin = {"html:testoutput/kogile.html","junit:testoutput/kogile.xml","json:testoutput/kogile.json"}
 )
 public class test_runner {
 
